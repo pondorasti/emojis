2b36a085d2cf225211748e541f138233f2ae20f7
Alexandru Turcanu <pondorasti@gmail.com>
2023-09-23 18:12:51 -0700

    feat: emoji server action

---
+++ src/app/emoji-form/submit-button.tsx
@@ -0,0 +1,20 @@
+import { CornerDownLeft, Loader2 } from "lucide-react"
+import React from "react"
+import { experimental_useFormStatus as useFormStatus } from "react-dom"
+
+export const SubmitButton = React.forwardRef<React.ElementRef<"button">>((_, ref) => {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      ref={ref}
+      type="submit"
+      disabled={pending}
+      aria-disabled={pending}
+      className="text-white rounded-full hover:bg-white/25 focus:bg-white/25 w-8 h-8 aspect-square flex items-center justify-center ring-0 outline-0"
+    >
+      {pending ? <Loader2 size={16} className="animate-spin" /> : <CornerDownLeft size={16} className="-ml-px" />}
+    </button>
+  )
+})
+SubmitButton.displayName = "SubmitButton"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "antialiased")}>{children}</body>
     </html>
   )
 }
