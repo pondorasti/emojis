5e78a35120af02284f207ac979ddd9dc1d8708af
Alexandru Turcanu <pondorasti@gmail.com>
2023-09-24 12:00:22 -0700

    feat: generate emoji with replicate

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : process.env.NGROK_URL
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
+import { customAlphabet } from "nanoid"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 7)
