05ff38e1e6025482cb6226a9264a3e2acd9f9dcc
Alexandru Turcanu <pondorasti@gmail.com>
2023-09-24 21:38:20 -0700

    feat: download emoji

--- src/app/_components/emoji-card/button-card.tsx
+++ src/app/_components/emoji-card/button-card.tsx
@@ -1,7 +1,12 @@
 "use client"
 
 import { EMOJI_SIZE } from "@/lib/constants"
+import { Download } from "lucide-react"
 import Image from "next/image"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { set } from "zod"
+import { Loader } from "../loader"
 
 interface ButtonCard {
   name: string
@@ -18,10 +23,13 @@ function downloadBlob(blobUrl: string, filename: string) {
 }
 
 export function ButtonCard({ name, src }: ButtonCard) {
-  async function handleDownload() {
-    try {
-      // setDownloading(true)
+  const [isDownloading, setIsDownloading] = useState(false)
 
+  async function handleDownload() {
+    setIsDownloading(true)
+    const toastId = toast.loading(`Downloading :${name}:`)
+
+    try {
       const res = await fetch(src, {
         headers: new Headers({ Origin: location.origin }),
         mode: "cors",
@@ -30,18 +38,17 @@ export function ButtonCard({ name, src }: ButtonCard) {
       const blobUrl = window.URL.createObjectURL(blob)
 
       downloadBlob(blobUrl, `${name}.png`)
+      toast.success(`Downloaded :${name}:`, { id: toastId })
     } catch (error) {
       console.error(error)
+      toast.error(`Failed to download :${name}:`, { id: toastId })
     } finally {
-      // setDownloading(false)
+      setIsDownloading(false)
     }
   }
 
   return (
-    <button
-      className="border flex flex-row flex-nowrap py-1 px-3 items-center shadow-sm rounded-xl gap-2 bg-white"
-      onClick={handleDownload}
-    >
+    <div className="borders ring-1 ring-gray-200 flex flex-row flex-nowrap py-1 px-2 items-center shadow-sm rounded-xl gap-1.5 bg-white w-full relative group">
       <Image
         alt="ai generated emoji"
         src={src}
@@ -50,7 +57,16 @@ export function ButtonCard({ name, src }: ButtonCard) {
         className="h-8 w-8 aspect-square"
       />
 
-      <p className="font-mono text-sm">:{name}:</p>
-    </button>
+      <p className="font-mono text-sm truncate">:{name}:</p>
+
+      <button
+        className="w-8 h-8 aspect-square flex items-center justify-center rounded-lg ring-1 ring-gray-200 absolute right-1 opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-out bg-white shadow focus:opacity-100"
+        onClick={handleDownload}
+        disabled={isDownloading}
+      >
+        <span className="sr-only">Download emoji</span>
+        {isDownloading ? <Loader /> : <Download size={16} />}
+      </button>
+    </div>
   )
 }
