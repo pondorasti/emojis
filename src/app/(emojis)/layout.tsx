a67a65e24b42312a98dffb483c9142ac4f2688d3
Alexandru Turcanu <pondorasti@gmail.com>
2023-09-26 23:28:10 -0700

    fix: force dynamic layout rendering

--- src/app/(emojis)/layout.tsx
+++ src/app/(emojis)/layout.tsx
@@ -11,6 +11,25 @@ import "./globals.css"
 import { DEFAULT_OG_IMAGE, PROD_URL } from "@/lib/constants"
 import { EmojiCount } from "./_components/emoji-count"
 
+/**
+ * Opt out of caching for all data requests in the route segment. Based on the docs,
+ * we should be **dynamically rendering** the page with **cached data**.
+ *
+ * With default config, the root page (/) is statically rendered and dynamic pages
+ * (/p/[id]) are dynamically rendered. This means that the root page will be cached
+ * and quickly start serving stale data, while the dynamic pages will be revalidated.
+ *
+ * From the docs:
+ *
+ * > If the segment is static (default), the output of the request will be cached and revalidated as part of the route segment.
+ * > If the segment is dynamic, the output of the request will not be cached and will be re-fetched on every request when the segment is rendered.
+ *
+ * Kinda cool, but a bit too much magic for me.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/rendering/server-components#dynamic-rendering
+ */
+export const dynamic = "force-dynamic"
+
 const BODY_PADDING = "px-4 sm:px-6"
 
 const inter = Inter({ subsets: ["latin"] })
