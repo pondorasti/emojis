fa04cb73541235c71de24172af8111aacb5817e9
Alexandru Turcanu <pondorasti@gmail.com>
2023-10-03 21:38:16 -0700

    fix: fallback favicon image for detail page

--- src/app/p/[id]/icon.tsx
+++ src/app/p/[id]/icon.tsx
@@ -1,4 +1,6 @@
 import { Favicon } from "@/app/_components/favicon"
+import { FEATURED_OG_IMAGES } from "@/lib/constants"
+import { getRandomItem } from "@/lib/utils"
 import { getEmoji } from "@/server/get-emoji"
 import { EmojiContextProps } from "@/server/utils"
 
@@ -8,5 +10,5 @@ export default async function Icon({ params }: EmojiContextProps) {
   const data = await getEmoji(params.id)
   if (!data) return
 
-  return Favicon({ url: data.noBackgroundUrl ?? "" })
+  return Favicon({ url: data.noBackgroundUrl ?? getRandomItem(FEATURED_OG_IMAGES) })
 }
