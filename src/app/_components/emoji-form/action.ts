75dac84f7dcf81b2c6600a982bcff9cdfcdc97a5
Alexandru Turcanu <pondorasti@gmail.com>
2023-10-04 21:27:51 -0700

    fix: emoji server action error handling (closes #6)

--- src/app/_components/emoji-form/action.ts
+++ src/app/_components/emoji-form/action.ts
@@ -1,34 +1,54 @@
 "use server"
 
-import { SITE_URL } from "@/lib/constants"
 import { nanoid } from "@/lib/utils"
 import { prisma } from "@/server/db"
 import { replicate } from "@/server/replicate"
+import { Ratelimit } from "@upstash/ratelimit"
+import { kv } from "@vercel/kv"
+import { jwtVerify } from "jose"
 import { redirect } from "next/navigation"
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  // 500 requests from the same IP in 1 day
+  limiter: Ratelimit.slidingWindow(500, "1 d"),
+})
 
 interface FormState {
   message: string
 }
 
 export async function createEmoji(prevFormState: FormState | undefined, formData: FormData): Promise<FormState | void> {
-  const rateLimitRes = await fetch(`${SITE_URL}/api/rate-limit`, {
-    headers: { Authorization: `Bearer ${process.env.API_SECRET}` },
-  })
-  if (!rateLimitRes.ok) return { message: "Too many requests, please try again later." }
-
   const prompt = (formData.get("prompt") as string | null)?.trim().replaceAll(":", "")
+  const token = formData.get("token") as string | null
+
   if (!prompt) return // no need to display an error message for blank prompts
-
   const id = nanoid()
-  const safetyRating = await replicate.classifyPrompt({ prompt })
-  const data = { id, prompt, safetyRating }
 
-  if (safetyRating >= 9) {
-    await prisma.emoji.create({ data: { ...data, isFlagged: true } })
-    return { message: "Nice try! Your prompt is inappropriate, let's keep it PG." }
+  try {
+    const verified = await jwtVerify(token ?? "", new TextEncoder().encode(process.env.API_SECRET ?? ""))
+    const ip = verified.payload.ip
+    if (typeof ip !== "string") throw new Error("IP not found in token payload")
+
+    const { remaining } = await ratelimit.limit(ip)
+    if (remaining <= 0) return { message: "Too many requests, please try again later." }
+
+    const safetyRating = await replicate.classifyPrompt({ prompt })
+    const data = { id, prompt, safetyRating }
+
+    if (safetyRating >= 9) {
+      await prisma.emoji.create({ data: { ...data, isFlagged: true } })
+      return { message: "Nice try! Your prompt is inappropriate, let's keep it PG." }
+    }
+
+    await Promise.all([prisma.emoji.create({ data }), replicate.createEmoji(data)])
+  } catch (error) {
+    console.error(error)
+    // @ts-expect-error
+    if (error.code === "ERR_JWS_INVALID") return { message: "Unauthorized" }
+
+    return { message: "Too many requests, please try again later." }
   }
-
-  await Promise.all([prisma.emoji.create({ data }), replicate.createEmoji(data)])
 
   redirect(`/p/${id}`)
 }
---
+++ src/app/api/token/route.ts
@@ -0,0 +1,19 @@
+import { Response } from "@/server/utils"
+import { SignJWT } from "jose"
+import { nanoid } from "nanoid"
+import { type NextRequest } from "next/server"
+
+export const runtime = "edge"
+
+export async function GET(request: NextRequest) {
+  const ip = request.ip ?? "127.0.0.1"
+  console.log({ ip })
+  const token = await new SignJWT({ ip })
+    .setProtectedHeader({ alg: "HS256" })
+    .setJti(nanoid())
+    .setIssuedAt()
+    .setExpirationTime("1d")
+    .sign(new TextEncoder().encode(process.env.API_SECRET ?? ""))
+
+  return Response.success({ body: { token } })
+}
