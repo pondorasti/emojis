72e898d6627a8722d5514220799f8b341f142f88
Alexandru Turcanu <pondorasti@gmail.com>
2023-10-04 21:27:51 -0700

    fix: emoji server action error handling (closes #6)

--- src/app/_components/emoji-form/index.tsx
+++ src/app/_components/emoji-form/index.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { createEmoji } from "./action"
 import { SubmitButton } from "./submit-button"
 // @ts-expect-error
 import { experimental_useFormState as useFormState } from "react-dom"
 import toast from "react-hot-toast"
+import useSWR from "swr"
 
 interface EmojiFormProps {
   initialPrompt?: string
@@ -14,11 +15,24 @@ interface EmojiFormProps {
 export function EmojiForm({ initialPrompt }: EmojiFormProps) {
   const [formState, formAction] = useFormState(createEmoji)
   const submitRef = useRef<React.ElementRef<"button">>(null)
+  const [token, setToken] = useState("")
 
   useEffect(() => {
     if (!formState) return
     toast.error(formState.message)
   }, [formState])
+
+  useSWR(
+    "/api/token",
+    async (url: string) => {
+      const res = await fetch(url)
+      const json = await res.json()
+      return json?.token ?? ""
+    },
+    {
+      onSuccess: (token) => setToken(token),
+    }
+  )
 
   return (
     <form action={formAction} className="bg-black rounded-xl shadow-lg h-fit flex flex-row px-1 items-center w-full">
@@ -35,6 +49,7 @@ export function EmojiForm({ initialPrompt }: EmojiFormProps) {
         placeholder="cat"
         className="bg-transparent text-white placeholder:text-gray-400 ring-0 outline-none resize-none py-2.5 px-2 font-mono text-sm h-10 w-full transition-all duration-300"
       />
+      <input aria-hidden type="text" name="token" value={token} className="hidden" />
       <SubmitButton ref={submitRef} />
     </form>
   )
