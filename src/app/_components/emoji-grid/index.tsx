d78c726fd47d81244444a0f696cfa31334d5dd15
Alexandru Turcanu <pondorasti@gmail.com>
2023-10-17 19:07:02 -0700

    feat: full text search

---
+++ src/app/_components/emoji-grid/index.tsx
@@ -0,0 +1,21 @@
+import { getEmojis } from "@/server/get-emojis"
+import { EmojiCard } from "../emoji-card"
+
+interface EmojiGridProps {
+  prompt?: string
+}
+
+export async function EmojiGrid({ prompt }: EmojiGridProps) {
+  const emojis = await getEmojis({ searchQuery: prompt })
+
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+      <h2 className="font-semibold text-md text-left w-full mb-3">{!!prompt ? "Related Emojis" : "Recent Emojis"}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-stretch w-full">
+        {emojis.map((emoji) => (
+          <EmojiCard key={emoji.id} id={emoji.id} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/server/get-emojis.ts
+++ src/server/get-emojis.ts
@@ -6,16 +6,25 @@ import { VALID_EMOJI_FILTER } from "./utils"
 
 export const getEmojis = cache(
   async (
-    { take, skip, orderBy }: { take?: number; skip?: number; orderBy?: Prisma.EmojiOrderByWithRelationInput } = {
+    {
+      take,
+      skip,
+      orderBy,
+      searchQuery,
+    }: { take?: number; skip?: number; orderBy?: Prisma.EmojiOrderByWithAggregationInput; searchQuery?: string } = {
       take: 100,
       skip: undefined,
       orderBy: { createdAt: Prisma.SortOrder.desc },
+      searchQuery: undefined,
     }
   ) =>
     prisma.emoji.findMany({
       select: { id: true, updatedAt: true },
       orderBy,
-      where: VALID_EMOJI_FILTER,
+      where: {
+        ...VALID_EMOJI_FILTER,
+        prompt: searchQuery ? { search: `${searchQuery}*` } : undefined,
+      },
       take,
       skip,
     })
