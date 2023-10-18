7c3cd55042ba6bb5d113fe969e4ba098b6ee2340
Alexandru Turcanu <pondorasti@gmail.com>
2023-10-17 19:07:02 -0700

    feat: full text search

---
+++ src/app/_components/emoji-grid/index.tsx
@@ -0,0 +1,30 @@
+import { getEmojis } from "@/server/get-emojis"
+import { EmojiCard } from "../emoji-card"
+
+interface EmojiGridProps {
+  prompt?: string
+}
+
+export async function EmojiGrid({ prompt }: EmojiGridProps) {
+  const emojis = await getEmojis({
+    take: 100,
+    orderBy: {
+      _relevance: {
+        fields: ["prompt"],
+        sort: "desc",
+        search: prompt ?? "",
+      },
+    },
+  })
+
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+      <h2 className="font-semibold text-md text-left w-full mb-3">{!!prompt ? "Related Emojis" : "Recent Emojis"}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-stretch w-full">
+        {emojis.map((emoji) => (
+          <EmojiCard key={emoji.id} id={emoji.id} />
+        ))}
+      </div>
+    </div>
+  )
+}
